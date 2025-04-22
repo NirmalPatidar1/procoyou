b9b45ff67ba42b20f859826e616980ae93c9f440
NirmalPatidar1 <nirmalpatidar.infograins@gmail.com>
2025-04-22 17:34:45 +0530

    update home url in project

--- procoyou/urls.py
+++ procoyou/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("<h1>Welcome to Procoyou App Home.</h1>")
 
 urlpatterns = [
+     path('', home),
     path('admin/', admin.site.urls),
     
     path('api/v1/', include('procoyouApi.urls')),
